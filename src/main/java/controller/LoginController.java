e49aa649b4fe505007f113b379c040d0994680a9
csythree <1160020938@qq.com>
2021-01-24 15:34:37 +0800

    csy-拉了基本的两个界面

---
+++ src/main/java/controller/LoginController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.control.TextField;
+
+public class LoginController {
+	@FXML
+	private TextField text_password;
+	@FXML
+	private TextField text_username;
+	@FXML
+	private Button btn_clear;
+	@FXML
+	private Button btn_login;
+
+}
