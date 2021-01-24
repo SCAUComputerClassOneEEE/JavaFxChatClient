7db6bb308eca495d086329160f61845bb5fb324d
csythree <1160020938@qq.com>
2021-01-24 21:46:16 +0800

    csy--建好controller

--- src/main/java/controller/ChatController.java
+++ src/main/java/controller/ChatController.java
@@ -1,4 +1,23 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.FlowPane;
+
 public class ChatController {
+
+    @FXML
+    private Button btn_send;
+
+    @FXML
+    private FlowPane messageList;
+
+    @FXML
+    private TextArea text_input;
+
+    @FXML
+    private ScrollPane scrollPane_history;
+
 }
