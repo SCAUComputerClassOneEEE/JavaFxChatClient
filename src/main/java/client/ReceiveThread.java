9664183ea38bdaa160e7d15468c61e26acf11d0f
Srenot <1054500767>
2021-01-25 10:59:16 +0800

    lbq socket接收发送类

--- src/main/java/client/ReceiveThread.java
+++ src/main/java/client/ReceiveThread.java
@@ -1,4 +1,70 @@
 package client;
 
-public class ReceiveThread {
+import controller.ChatController;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+
+public class ReceiveThread extends Thread {
+    public static Socket socket;
+
+    public void run() {
+        try {
+            InputStream in = socket.getInputStream();
+            BufferedReader reader;
+            reader = new BufferedReader(new InputStreamReader(in));
+            String str;
+            while (true) {
+                str = reader.readLine();
+                if (str != null) {
+                    if (str.startsWith("Y")) {
+                        updataChat(str);
+                    }
+                    else if (str.startsWith("N")) {
+                        logIn(str);
+
+                    }else if (str.startsWith("W")){//下线信息
+                        offLine(str);
+                    }
+                }
+            }
+
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void logIn(String str) {
+        if(str.equals("N/N")){
+            //显示登录失败信息
+        }else {
+            str = str.substring(2,str.length());//删除标识N
+            //登录成功，显示上线信息
+        }
+    }
+
+    private void offLine(String str) {
+        str = str.substring(2,str.length());//删除标识W
+        //显示下线信息
+    }
+
+    private void updataChat(String str) {
+        str = str.substring(2,str.length());//删除标识Y
+        String[] message = str.split("//");
+        //更新信息界面
+    }
+
+
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        socket = new Socket("169.254.40.2",6666);//端口号？
+
+    }
 }
--- src/main/java/client/SendThread.java
+++ src/main/java/client/SendThread.java
@@ -1,4 +1,85 @@
 package client;
 
-public class SendThread {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class SendThread extends Thread {
+    public static Socket socket;
+    private int type;//1是发送登录信息，2是发送聊天信息，3是发送下线信息
+    private String userName = null;
+    private String password = null;
+    private String user1 = null;
+    private String user2 = null;
+    private String message = null;
+
+    //利用构造函数来传递线程的参数
+    public SendThread(int typeID,String userName_,String password_){//登录时使用的构造函数
+        this.type = typeID;
+        this.userName = userName_;
+        this.password = password_;
+    }
+    public SendThread(int typeID,String user1_,String user2_,String message_){//发送信息时使用的构造函数
+        this.type = typeID;
+        this.user1 = user1_;
+        this.user2 = user2_;
+        this.message = message_;
+    }
+    public SendThread(int typeID,String user1_){//登出时使用的构造函数
+        this.type = typeID;
+        this.user1 = user1_;
+    }
+
+    public void run(){
+        if(type == 1){
+            sendLogIn();
+        }else if (type == 2){
+            sendMessage();
+        }else if (type == 3) {
+            sendLogOut();
+        }
+    }
+
+    private void sendLogOut() {
+        try {
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.write("W/"+user1+"\n");
+            pw.flush();
+
+        }catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    private void sendMessage() {
+        try {
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.write("Y/"+user1+"/"+user2+"/"+message+"\n");
+            pw.flush();
+
+        }catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private void sendLogIn() {
+        try {
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            pw.write("N/"+userName+"/"+password+"\n");
+            pw.flush();
+
+        }catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException{
+
+                socket = new Socket("169.254.40.2", 6666);
+
+        }
 }
